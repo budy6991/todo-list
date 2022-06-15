b0b59e562bade08389aa5975fe327019ce5da19d
budy6991 <alvmenfer@gmail.com>
2022-06-15 14:23:23 +0200

    Issue with pushing the todos fixed

--- src/displayModules.js
+++ src/displayModules.js
@@ -23,23 +23,37 @@ let display = {
         projectName.textContent = name
 
         project.append(projectName, addTodo, removeProject)
+        getElements.body().appendChild(project)
 
         project.dataset.identifier = name 
         addTodo.dataset.identifier = name 
 
         let identifier
 
-        getElements.body().appendChild(project)
 
         addTodo.onclick = function (){
 
         getElements.todoModal().showModal()
+        getElements.submitTodo().onclick = function (){
+
+            createProjects.arrayOfProjects.forEach(project => {
+
+                if (project.name === addTodo.dataset.identifier) {
+                    
+                    let todo = createProjects.createTodo(getElements.todoName(), getElements.todoNotes(), getElements.todoDate(), getElements.todoPriority())
+
+                    project.storeTodos(todo)
+                    
+                    
+                    console.log(createProjects.arrayOfProjects)
+                    
+                }
+            })
+        }
             
            identifier = createProjects.arrayOfProjects.filter(project => project.name === addTodo.dataset.identifier)
 
            identifier = identifier[0]
-
-           
            
         }
         removeProject.onclick = function (){
@@ -51,23 +65,7 @@ let display = {
             }} )   
         }
 
-        getElements.submitTodo().onclick = function (){
-
-            createProjects.arrayOfProjects.forEach(project => {
-
-                if (project.name === addTodo.dataset.identifier) {
-                    
-                    let todo = createProjects.createTodo(getElements.todoName(), getElements.todoNotes(), getElements.todoDate(), getElements.todoPriority())
-
-                    
-                    console.log(project.name)
-                    console.log(addTodo.dataset.identifier)
-                    
-                    // console.log(createProjects.arrayOfProjects)
-                    
-                }
-            })
-        }
+        
     }
 }
 
