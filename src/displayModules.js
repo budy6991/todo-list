2fdd94f2c17d3edf36df65e760fb7db6e6337c69
budy6991 <alvmenfer@gmail.com>
2022-06-16 14:39:44 +0200

    Todos able to display

--- src/displayModules.js
+++ src/displayModules.js
@@ -17,24 +17,19 @@ let display = {
         let addTodo = document.createElement('button')
         let expandProject = document.createElement('button')
         let removeProject = document.createElement('button')
-        
-
         let todoContainer = document.createElement('div')
-        let closeContainer = document.createElement('button')
-        let todoCard = document.createElement('div')
-        let todoName = document.createElement('div')
-        let todoNotes = document.createElement('div')
-        let todoDate = document.createElement('div')
-        let todoPriority = document.createElement('div')
-        let todoExpand = document.createElement('button')
-        let todoRemove = document.createElement('button')
 
+        let closeContainer = document.createElement('button')
+
+        
 
         addTodo.classList.add('add-todo')
         removeProject.classList.add('remove-project')
         project.classList.add('project-card')
+        
         expandProject.classList.add('expand-project')
         todoContainer.classList.add('todo-container')
+
         closeContainer.classList.add('close-container')
 
         removeProject.textContent = 'Remove'
@@ -43,6 +38,7 @@ let display = {
         projectName.textContent = name
 
         closeContainer.textContent = 'CLOSE'
+        
 
         todoContainer.append(closeContainer)
         
@@ -54,39 +50,62 @@ let display = {
 
 
         addTodo.onclick = function (){
+            
+            
 
             getElements.todoModal().showModal()
             getElements.submitTodo().onclick = function (){
                 
                 createProjects.arrayOfProjects.forEach(project => {
-                    
+
                     if (project.name === addTodo.dataset.identifier) {
                         
                         let todo = createProjects.createTodo(getElements.todoName(), getElements.todoNotes(), getElements.todoDate(), getElements.todoPriority())
-                        
-                        // todoName.textContent = todo.name
-                        // todoCard.appendChild(todoName)
-                        // todoContainer.appendChild(todoCard)
-
+                    
                         project.storeTodos(todo)
+
+                        
+                        let todoCard = document.createElement('div')
+                        let todoName = document.createElement('div')
+                        let todoNotes = document.createElement('div')
+                        let todoDate = document.createElement('div')
+                        let todoPriority = document.createElement('div')
+                        let todoExpand = document.createElement('button')
+                        let todoRemove = document.createElement('button')
+
+
+                        todoExpand.textContent  = 'Expand'
+                        todoRemove.textContent = 'X'
+                        todoCard.classList.add('todo-card')
+
+                        todoName.append(`Name: ${todo.name}`)
+                        todoNotes.append(`Notes: ${todo.notes}`)
+                        todoDate.append(`Date: ${todo.date}`)
+                        todoPriority.append(`Priority: ${todo.priority}`)
+
+                        todoCard.append(todoName,todoNotes,todoDate, todoPriority, todoExpand, todoRemove)
+                        
+                        todoContainer.append(todoCard)
+
                                                 
                         console.log(createProjects.arrayOfProjects)
 
                     }
 
                     expandProject.onclick = function (){
-                        getElements.body().appendChild(todoContainer)
-                    }
 
-                    console.log(todo)
+                        project.todos.forEach(todo => {
+
+                            getElements.body().appendChild(todoContainer)
+                
+                        })
+                         
+                    }
+                        
                 })
 
-                
-
             }
-            
-            
-            
+        
         }
         removeProject.onclick = function (){
             getElements.body().removeChild(project)
